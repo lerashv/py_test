4c6d925ecc5c6786a088a587814df7050c70d648
Valeria Shvadronova <lerashv@gmail.com>
2017-10-24 12:41:02 +0300

    first upload

---
+++ hi.py
@@ -0,0 +1,48 @@
+print('hi')
+digit = 6
+word = "bsuir"
+print(digit)
+one, two, three = 1, "two", 3.0
+print(one)
+print(two)
+print(three)
+numb = 1
+str = "char"
+numb = str
+print(numb) #comment
+
+
+test_1 = '12'
+test_2 = 12
+if test_1 and test_2:
+    print('vars {} {}'.format(test_1, test_2))
+if test_1 or test_2:
+    print("ok")
+
+print(type('hello'))  
+print(len('hello'))  
+mystr='{}_{}'.format('bsuir', 'govno')
+print(mystr)
+print(mystr[0])
+print(mystr[-3])
+print(mystr[0:5])
+print(mystr[0:-2])
+
+print(3**5)
+print(25.5%5)
+
+print(20^17)
+
+def func():
+	print('function1')
+
+func()
+
+def func1():
+	return "this is a result"
+
+print(func1())	
+
+res=input('enter a digit')
+print(res)
+
